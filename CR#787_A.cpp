5ffea8ffe6b37c987b2f4e4f8c7e62a585e97cbb
jbjzeehad <ilamta.97@gmail.com>
2022-05-05 22:55:21 +0600

    First Online problem solved

---
+++ CR#787_A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	long long int x,y,a,b,c,t,sum;
+cin>>t;
+while(t--){
+	cin>>a>>b>>c>>x>>y;
+	x=x-a;
+	if(x<0){
+		x=0;
+	}
+	y=y-b;
+	if(y<0){
+		y=0;
+	}
+	sum=x+y;
+	if(sum<=c){
+		cout<<"YES"<<endl;
+	}
+	else
+		cout<<"NO"<<endl;
+	}
+}
